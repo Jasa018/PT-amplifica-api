3248b76fe4c74ca80b0d2360bd6d26391437be43
Joaquín <joaquin.alamiro@gmail.com>
2025-10-22 18:23:19 -0300

    fix(setup): fix migration

--- setup.bat
+++ setup.bat
@@ -9,17 +9,22 @@ docker run --rm -v "%cd%:/app" -w /app laravelsail/php83-composer:latest compose
 echo "Levantando los contenedores de Docker..."
 docker-compose up -d
 
-echo "Esperando a que la base de datos este lista (30 segundos)..."
-timeout /t 30 /nobreak > nul
+echo "Esperando a que la base de datos este lista..."
+:loop
+FOR /f "tokens=*" %%g IN ('docker inspect --format "{{.State.Health.Status}}" pt-amplifica-api-mysql-1') do (SET status=%%g)
+IF NOT "%status%"=="healthy" (
+    timeout /t 5 /nobreak > nul
+    GOTO loop
+)
+echo "La base de datos esta lista!"
 
 echo "Ejecutando migraciones de la base de datos..."
-docker-compose exec laravel.test php artisan migrate
+docker-compose exec laravel.test php artisan migrate:fresh --seed
 
 echo "Limpiando la cache de configuracion..."
 docker-compose exec laravel.test php artisan config:clear
 
-echo "Creando usuario de prueba..."
-docker-compose exec laravel.test php artisan tinker --execute="App\Models\User::factory()->create(['email' => 'test@example.com', 'password' => bcrypt('password')])"
+
 
 echo "Setup finalizado!"
 echo "Puedes iniciar sesion con el usuario: test@example.com y la contrasena: password"
